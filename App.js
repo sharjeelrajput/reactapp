e01a5b3fa924137e0233b95035e49e9ce16d390a
sharjeel@truemeridian.com <MHAgithub1!>
2022-07-15 16:48:27 +0500

    first page

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Linking, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Button title=" YouTube Channel" onPress={() => {
+        Linking.openURL("http://www.youtube.com/watch")
+      }}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccc',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text : {
+    margin: 10
+  }
 });
